8cdbfe194beae1288ab5ff7593b5228fd2978dce
haamid10 <apdixamiid919@gmail.com>
2022-12-14 19:27:40 +0300

    14 dec

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router,Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+const App = () => {
+  return (
+    <Router>
+         <div className="flex flex-col justify-between h-screen">
+          <Navbar/>
+          <main></main>
+         </div>
+    </Router>
+      
+  )
+}
+
+export default App
---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  const footerYear= new Date().getFullYear()
+  return (
+    <div>
+        {/* jkfjf */}
+    </div>
+  )
+}
+
+export default Footer
