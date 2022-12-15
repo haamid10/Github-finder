636ad9ea0958402680e7837eb09d111e01149d04
haamid10 <apdixamiid919@gmail.com>
2022-12-15 11:47:39 +0300

    aded Page folder and some links

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+        <h1 className="text-6xl mb-4">
+          Github Finder
+        </h1>
+        <p className="mb-4 text-2xl font-light">
+          A React app to Search github profiles and see Profile details
+        </p>
+    </div>
   )
 }
 
