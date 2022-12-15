c4f52c7fcefc33a4255eff618c5de1fa5a4e76b9
haamid10 <apdixamiid919@gmail.com>
2022-12-15 11:31:47 +0300

    aded Page folder and some links

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { BrowserRouter as Router,Route } from "react-router-dom"
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom"
+import  About from './components/pages/About'
+import  Home from './components/pages/Home'
+import  NotFound from './components/pages/NotFound'
 import Navbar from "./components/layout/Navbar"
 import  Footer from "./components/layout/Footer"
 const App = () => {
   return (
     <Router>
          <div className="flex flex-col justify-between h-screen">
+        
           <Navbar/>
-          <main className="container mx-auto px-3 pb-12 ">Content </main>
+          <main className="container mx-auto px-3 pb-12 ">
+          <Home/>
+            <About/>
+            <NotFound/>
+          <Routes>
+            
+
+          </Routes>
+           </main>
           <Footer/>
          </div>
     </Router>
