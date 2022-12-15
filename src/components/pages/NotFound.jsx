292dbce7b678097c5cb456e921e14ed92adcd880
haamid10 <apdixamiid919@gmail.com>
2022-12-15 12:00:44 +0300

    aded pages some styles in Daisy UI

--- src/components/pages/NotFound.jsx
+++ src/components/pages/NotFound.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import { FaHome } from "react-icons/fa"
+import { Link } from "react-router-dom"
 const NotFound = () => {
   return (
-    <div>
+    <div className="hero">
+      <div className="text-center hero-content">
+        <div className="max-w-lg">
+          <h1 className="text-8xl font-bold mb-8 ">
+              Oops!
+          </h1>
+        </div>
+        <p className="text-5xl mb-8">
+          404 -page not found!
+        </p>
+        <Link to='/' className=" btn btn-primary btn-lg">
+          <FaHome  className="mx-2"/>Back Home
+        </Link>
+      </div>
       not found
     </div>
   )
