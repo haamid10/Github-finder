9256e6030d41f42d157ce10cadb4a744b5f62169
haamid10 <apdixamiid919@gmail.com>
2022-12-24 19:45:09 +0300

    tring to add loader

--- src/components/USERS/UserResult.jsx
+++ src/components/USERS/UserResult.jsx
@@ -20,13 +20,19 @@ const UserResult = () => {
     setUsers(data)
     setLoading(false)
    }
-  return (
-    <div className=' grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 '>
-      {users.map((user)=>(
-        <h3>{user.login}</h3>
-      ))}
-    </div>
-  )
-}
  
+   if(!Loading){
+    return (
+      <div className=' grid grid-cols-1 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 '>
+        {users.map((user)=>(
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+      )
+   }
+ else{
+  return <h3>Loading .......</h3>
+ }
+  
+}
 export default UserResult
