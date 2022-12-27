d1ac22988bf00e304fb84fbe191d4fd04fcfa8a5
haamid10 <apdixamiid919@gmail.com>
2022-12-27 20:47:11 +0300

    displaying users

--- src/components/USERS/UserItem.jsx
+++ src/components/USERS/UserItem.jsx
@@ -1,10 +1,13 @@
-
-function UserItem() {
+import PropTypes from "prop-types"
+function UserItem({user: {login, avatar_url}}) {
   return (
     <div>
       user
     </div>
   )
+}
+UserItem.PropTypes ={
+  user: PropTypes.object.isRequired,
 }
 
 export default UserItem
