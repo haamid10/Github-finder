7d95ce222065719d6ef8d7a2e3b04da4cb008453
haamid10 <apdixamiid919@gmail.com>
2022-12-27 19:36:24 +0300

    addin spiinner loader

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,11 @@
+import Spinner from "./assets/spinner.gif"
+
+const Spinner = () => {
+  return (
+    <div className="w-100 mt-100">
+      <img src={Spinner} width={180}    className="text-center mx-auto" alt="Loading..."/>
+    </div>
+  )
+}
+
+export default Spinner
