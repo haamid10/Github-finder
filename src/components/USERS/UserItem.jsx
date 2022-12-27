04c447484d792d323bba2473af2523b49b5b685a
haamid10 <apdixamiid919@gmail.com>
2022-12-27 20:55:21 +0300

    displaying users

--- src/components/USERS/UserItem.jsx
+++ src/components/USERS/UserItem.jsx
@@ -1,7 +1,6 @@
 import PropTypes from "prop-types"
 function UserItem({user: {login, avatar_url}}) {
-  return (
-    <div className="card shadow-md compact side bg-base-100
+  return <div className="card shadow-md compact side bg-base-100
     ">
       <div className="flex-row items-center space-x-4 card-body">
         <div>
@@ -10,10 +9,11 @@ function UserItem({user: {login, avatar_url}}) {
             <img src={avatar_url} alt="" />
           </div>
         </div>
+      <h2>{login }</h2>
       </div>
-      user
     </div>
-  )
+  
+  
 }
 UserItem.propTypes ={
   user: PropTypes.object.isRequired,
